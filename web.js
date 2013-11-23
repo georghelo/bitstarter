53e6b77a1b63c4d2d90af4f6972ca0ddfa70b2f8
georghelo <georgehelo@gmail.com>
2013-11-23 01:50:39 +0000

    Updated index.html

--- web.js
+++ web.js
@@ -7,10 +7,9 @@ app.get('/', function(request, response) {
     var buffer = fs.readFileSync('./index.html');
     var str = buffer.toString();
     response.send(str);
-    // response.send('Hello World 2!');
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
